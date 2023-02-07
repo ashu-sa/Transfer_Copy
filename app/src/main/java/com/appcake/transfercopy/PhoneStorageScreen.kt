2b3809ce492685b8777d4d459e0712be25b9e895
Ashutosh Sahoo <sahooashutosh222@gmail.com>
2023-02-07 11:01:20 +0530

    Search Bar issue fixed

--- app/src/main/java/com/appcake/transfercopy/PhoneStorageScreen.kt
+++ app/src/main/java/com/appcake/transfercopy/PhoneStorageScreen.kt
@@ -1,5 +1,6 @@
 package com.appcake.transfercopy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
@@ -47,6 +48,21 @@ class PhoneStorageScreen : AppCompatActivity() {
         setProgressBar(totalSpaceInt,remainingSpaceInt)
 
         binding.videoSpaceText.text = formatSize(videoSpace())
+
+        binding.apply {
+            contactsLinearLayout.setOnClickListener {
+                val intent = Intent(this@PhoneStorageScreen,ContactActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            docsLinearLayout.setOnClickListener {
+                val intent = Intent(this@PhoneStorageScreen,DocsActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+        }
 
 
 
